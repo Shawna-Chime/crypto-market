cb2243ae7bb6d2ea6d9d478947e4f9cd8a9fc543
Shawna Chime <shawnachime36@gmail.com>
2025-01-14 11:36:25 +0100

    -

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  $(".ri-menu-fill").click(function () {
+    $(".mobile-menu").show();
+    $(".mobile-menu").animate({ right: "0px" });
+  });
+
+  $(".ri-close-line").click(function () {
+    $(".mobile-menu").hide();
+  });
+
   $(".top-partners-slider").slick({
     infinite: true,
     slidesToShow: 5,
@@ -7,6 +16,36 @@ $(document).ready(function () {
     autoplaySpeed: 3000,
     prevArrow: '<span class="arrow prev-arrow"></span>',
     nextArrow: '<span class="arrow next-arrow"></span>',
+    responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+          arrows: false,
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          arrows: false,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 559,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 407,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".roadmap-slider-container").slick({
     infinite: true,
@@ -16,6 +55,29 @@ $(document).ready(function () {
     autoplaySpeed: 3000,
     prevArrow: '<i class="ri-arrow-left-line round-btn-primary"></i>',
     nextArrow: '<i class="ri-arrow-right-line round-btn-primary"></i>',
+    responsive: [
+      {
+        breakpoint: 1074,
+        settings: {
+          arrows: true,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 813,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 486,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".our-partners-slider-container").slick({
     infinite: true,
@@ -25,6 +87,22 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
     prevArrow: false,
     nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".press-us-slider-container").slick({
     infinite: true,
@@ -34,5 +112,14 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
     prevArrow: false,
     nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
